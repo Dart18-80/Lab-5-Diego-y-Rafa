e553f6d418750785988fbd26bf863ab89f201db9
Diego Ruiz <diegoruiztrabanino13@gmail.com>
2021-04-22 20:42:43 -0600

    Agregar archivos de proyecto.

---
+++ Lab 5 Diego y Rafa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_5_Diego_y_Rafa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
