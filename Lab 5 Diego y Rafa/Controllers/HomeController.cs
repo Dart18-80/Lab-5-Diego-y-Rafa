e63fae88fb7456b7d9b31278e89c4872bce334e8
randres <randr@DESKTOP-DVJNG83>
2021-04-24 19:50:22 -0600

    Login

--- Lab 5 Diego y Rafa/Controllers/HomeController.cs
+++ Lab 5 Diego y Rafa/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Lab_5_Diego_y_Rafa.Models;
 using Microsoft.AspNetCore.Http;
+using Lab_5_Diego_y_Rafa.Helpers;
 
 namespace Lab_5_Diego_y_Rafa.Controllers
 {
@@ -21,9 +22,52 @@ namespace Lab_5_Diego_y_Rafa.Controllers
 
         public IActionResult Index()
         {
+
             return View();
         }
-
+        [HttpPost]
+        public IActionResult Index(IFormCollection collection)
+        {
+            try
+            {
+                int contador = 0;
+                string e = null;
+                var NuevoCliente = new Models.Cliente
+                {
+                    NombreUsuario = collection["NombreUsuario"],
+                    Conntraseña = collection["Conntraseña"],
+                };
+                if (Singleton.Instance.TablaUsuario.Count==0)
+                {
+                    e = "Cree un Usuario";
+                    ViewData["ErrorUusario"] = e;
+                    return View();
+                }
+                else
+                {
+                    while (Singleton.Instance.TablaUsuario.Count>contador)
+                    {
+                        if (Singleton.Instance.TablaUsuario[contador].NombreUsuario==NuevoCliente.NombreUsuario && Singleton.Instance.TablaUsuario[contador].Conntraseña == NuevoCliente.Conntraseña)
+                        {
+                            return RedirectToAction("Tarea");
+                        }
+                        else
+                        {
+                            contador++;
+                        }
+                    }
+                    e = "Nombre de Usuario o Conntraseña Incorrecta";
+                    ViewData["ErrorUusario"] = e;
+                    return View();
+                }
+               
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+       
+        }
         public IActionResult Privacy()
         {
             return View();
@@ -31,6 +75,56 @@ namespace Lab_5_Diego_y_Rafa.Controllers
         public IActionResult Tarea() 
         {
             return View();
+        }
+        public IActionResult CrearUsuario()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CrearUsuario(IFormCollection collection)
+        {
+            try
+            {
+                string ErrorC = null;
+                var NuevoCliente = new Models.Cliente
+                {
+                    NombreUsuario = collection["NombreUsuario"],
+                    Conntraseña = collection["Conntraseña"],
+                    Cargo = collection["Cargo"]
+                };
+                if (Singleton.Instance.TablaUsuario.Count == 0)
+                {
+                    Singleton.Instance.TablaUsuario.Add(NuevoCliente);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    for (int i = 0; i < Singleton.Instance.TablaUsuario.Count; i++)
+                    {
+                        if (Singleton.Instance.TablaUsuario[i].NombreUsuario == NuevoCliente.NombreUsuario)
+                        {
+                            ErrorC = "El Nombre de usuario seleccionado ya existe: ESCRIBA OTRO PORFAVOR";
+                                 ViewData["ErrorCliente"] = ErrorC;
+                            return View();
+                        }
+                        else
+                        {
+                            ErrorC = "";
+                            ViewData["ErrorCliente"] = ErrorC;
+                            Singleton.Instance.TablaUsuario.Add(NuevoCliente);
+                            i += Singleton.Instance.TablaUsuario.Count - i;
+                            return RedirectToAction("Index");
+                        }
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+            
         }
         [HttpPost]
 
---
+++ Lab 5 Diego y Rafa/Helpers/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab_5_Diego_y_Rafa.Models;
+
+namespace Lab_5_Diego_y_Rafa.Helpers
+{
+    public class Singleton
+    {
+        private static Singleton _instance = null;
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Singleton();
+                return _instance;
+            }
+        }
+        public List<Cliente> TablaUsuario = new List<Cliente>();
+    }
+}
